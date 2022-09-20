0937570857640823eaf52b994ef475a2024120e2
bitspa <112925221+bitspa@users.noreply.github.com>
2022-09-20 16:41:57 +0530

    Create exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,18 @@
+
+function start1(){
+    console.log("The numbers from 1 to 10 are as follows:");
+    for(let i=1;i<=10;i++)
+    {
+        console.log(i);
+    }
+}
+start1();
+//......................................................................//....................................
+function start(){
+    var str="hello";
+    for(let i=str.length-1;i>=0;i--)
+    {
+        console.log(str[i]);
+    }
+}
+start(); 
