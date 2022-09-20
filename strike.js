c12f37f8bb4ee487e2c2477d6bfd2ee022b2e8d2
bitspa <112925221+bitspa@users.noreply.github.com>
2022-09-20 16:43:38 +0530

    Create strike.js

---
+++ strike.js
@@ -0,0 +1,14 @@
+
+var modal = document.getElementById("myModal");
+
+
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+    span.onclick = function() {
+        modal.style.textDecoration = 'line-through';
+          }
+
