a1fb8e7cc6796ba06d11bfe0e0ba8f66d8887869
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-01 16:13:54 +0530

    Update todo2.js

--- todo2.js
+++ todo2.js
@@ -1,15 +1,20 @@
 import React from 'react';  
+import { useState } from 'react';
 const ToDoList=(props)=>{
+  const[line,setLine]= useState(false);
+    const cutIt=()=>{
+     setLine(true);
+    };
 return (
 <>
 <div className="todo_style" >
-<button className="fa-items" 
-onClick={
-  ()=>{props.onSelect(props.id)}
-  }>x</button>
-<li>{props.text }</li>
+<button className="fa-items" onClick={cutIt} 
+ >x</button>
+<li style={{textDecoration : line?'line-through': 'none'}}>{props.text }</li>
 </div>
 </>
-);
+); 
 };
 export default ToDoList;
+
+
