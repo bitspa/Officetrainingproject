32a09f7987a29443fad88bafd9f6fbea4375e6b2
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-01 15:19:18 +0530

    Create todo1.js

---
+++ todo1.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react';
+import TodoList from './components/ToDoList/ToDoList';
+import './App.css';
+function App() {
+  const [inputList,setInputList]=useState("Buy Apple"); 
+  const[Items,setItems]=useState([]);
+  const itemEvent=(event)=>{
+    setInputList(event.target.value);
+  };
+  const listOfItems=()=>{
+    setItems((oldItems)=>
+    {
+    return [...oldItems,inputList];  
+    });
+    setInputList("");
+  };
+  const deleteItems=(id)=>{
+    console.log('deleted');
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+         return index!==id;
+      });
+    })
+  }   
+  return (
+    <> 
+ <div className="main_div">
+  <div className="center_div">
+    <br/>
+    <h1>ToDoList</h1>
+    <br/>
+    <input type="text" placeholder="Add a item" onChange={itemEvent} value={inputList}/>
+    <button onClick={listOfItems}>+</button>
+    <ol>
+      {/* <li>{inputList}</li> */}
+          {Items.map((itemval,index)=>{
+          return <TodoList key ={index} id={index} text ={itemval} 
+          onSelect={deleteItems}/>
+          })}
+    </ol> 
+ </div>
+ </div>
+    </>
+  );
+}
+export default App;
+
