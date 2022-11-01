c92477a2d60a3d382a5c9591e3e3df2fe84c86f1
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-01 15:20:17 +0530

    Create todo2.js

---
+++ todo2.js
@@ -0,0 +1,15 @@
+import React from 'react';  
+const ToDoList=(props)=>{
+return (
+<>
+<div className="todo_style" >
+<button className="fa-items" 
+onClick={
+  ()=>{props.onSelect(props.id)}
+  }>x</button>
+<li>{props.text }</li>
+</div>
+</>
+);
+};
+export default ToDoList;
