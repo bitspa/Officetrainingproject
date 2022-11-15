792007fa19044a3a77ed6563821478f1aa79198d
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-15 15:25:29 +0530

    Create App(Mui).js

---
+++ App(Mui).js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './components/Loginform/Loginform';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
