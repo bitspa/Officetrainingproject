1e81843edfc56569dd208bd04d917de0fb049651
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-15 15:19:11 +0530

    Create App(StyleComponent).js

---
+++ App(StyleComponent).js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button2 from './components/Button/Button';
+import Navbar2 from './components/Navbar/NavbaR';
+import Paragraph2 from './components/Paragragh/Paragragh';
+export class App extends Component {
+render(){
+return(
+  <>
+ <Navbar2/>
+ <Paragraph2/>
+ <Button2/>
+ </>
+)
+}
+}
+export default App;
