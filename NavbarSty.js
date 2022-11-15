c7d70f22132fb7ff615bda5ec1c10b1fd1588740
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-15 15:24:41 +0530

    Create NavbarSty.js

---
+++ NavbarSty.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+export const Navbar = styled.nav`
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #5F9EA0;
+  }
+  
+  li {
+    float: right;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  li a:hover {
+    background-color: #6082B6;
+  }
+`;
