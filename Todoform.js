13f10597f265a371fccd5d0a23094db040df5b9e
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-16 14:28:59 +0530

    Create Todoform.js

---
+++ Todoform.js
@@ -0,0 +1,40 @@
+import React from "react";
+import shortid from "shortid";
+import {Button} from '../Styled/ButtonSty'
+export default class TodoForm extends React.Component {
+  state = {
+    text: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit({
+      id: shortid.generate(),
+      text: this.state.text,
+      complete: false
+    });
+    this.setState({
+      text: ""
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="text"
+          value={this.state.text}
+          onChange={this.handleChange}
+          placeholder="write something....."
+        />
+        <Button secondary onClick={this.handleSubmit}>Add Todo</Button>
+      </form>
+    );
+  }
+}
