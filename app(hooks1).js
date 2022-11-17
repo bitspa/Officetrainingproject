fa75cc5c9f16a1261881b527780a349305c2892b
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-17 11:22:24 +0530

    Create app(hooks1).js

---
+++ app(hooks1).js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import EffectTutorial from './Components/UseEffect'
+function App() {
+  return (
+    <>
+<EffectTutorial/>
+</>
+  );
+}
+export default App;
