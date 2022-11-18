9ec86f670124c1fc28bc04eb12d3df8a3c702732
bitspa <112925221+bitspa@users.noreply.github.com>
2022-11-18 12:24:37 +0530

    Create app(customhook).js

---
+++ app(customhook).js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import EffectTutorial from './Components/useCustomFetch'
+function App() {
+  return (
+    <>
+<EffectTutorial/>
+</>
+  );
+}
+export default App;
